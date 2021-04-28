946016f2d24a7af3f7a93c80b6140851aeeba7da
nileshb <nileshbari.8085@gmail.com>
2021-04-29 01:07:57 +0530

    code refactor

--- resources/views/pages/view-designer.blade.php
+++ resources/views/pages/view-designer.blade.php
@@ -12,7 +12,7 @@
           <div class="card">
              <div class="designerImg">
                 @if($designer->display_picture)
-                 <img src="{{ asset('/default/designer/display_picture/'. $designer->display_picture) }}" class="img-fluid" alt="Cover">
+                 <img src="{{ asset('/uploads/designer/display_picture/'. $designer->display_picture) }}" class="img-fluid" alt="Cover">
                 @else
                 <img src="{{ asset('/default/user.png') }}" class="img-fluid" alt="Cover">
                 @endif
