cc935f0909b36ab1cca7d0b1d4cacbc06cef731b
nileshb <nileshbari.8085@gmail.com>
2021-05-03 21:15:07 +0530

    code refactor

--- resources/views/designer/draft.blade.php
+++ resources/views/designer/draft.blade.php
@@ -16,7 +16,7 @@
             </div>
             <div class="card-body p-3">
                 <div class="d-flex align-items-center mb-2">
-                    @if($design->designer()->display_picture)
+                    @if($design->designer->display_picture)
                     <div class="author-img">
                     <img alt="Person" class="img-fluid rounded-circle mr-1" src="{{ asset('default/user.png') }}" style="width:35px">
                     </div>
