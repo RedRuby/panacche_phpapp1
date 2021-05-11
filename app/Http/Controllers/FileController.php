d3d01c5785cd6b6ea8274ecca6173cd85ef863b1
nileshb <nileshbari.8085@gmail.com>
2021-05-11 18:35:30 +0530

    download files

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -9,12 +9,28 @@ class FileController extends Controller
 {
     function getResumeFile($filename){
         //$file=Storage::disk('public')->get($filename);
-        $path=public_path()."/uploads/designer/resume/".$filename;
-        return response()->download($path, $filename);
+        if(strpos($filename, "http://") || strpos($filename, "http://") !== false){
+            $path = $filename;
+            $basename = basename($path, ".");
+
+            return response()->download($path, $basename);
+        }else{
+            $path=public_path()."/uploads/designer/resume/".$filename;
+            return response()->download($path, $filename);
+        }
+
     }
 
     function getPortfolioFile($filename){
-        $path=public_path()."/uploads/designer/portfolio/".$filename;
-        return response()->download($path, $filename);
+
+        if(strpos($filename, "http://" || strpos($filename, "http://")) !== false){
+            $path = $filename;
+            $basename = basename($path, ".");
+
+            return response()->download($path, $basename);
+        }else{
+            $path=public_path()."/uploads/designer/portfolio/".$filename;
+            return response()->download($path, $filename);
+        }
     }
 }
